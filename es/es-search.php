e5746f6b62f2b76c43e01878c22ef9a0ff8f117b
GeGe <ptre.chez.gg@gmail.com>
2023-09-12 21:55:59 +0200

    fix(lang): Fix typo

--- es/es-search.php
+++ es/es-search.php
@@ -1,7 +1,7 @@
     	"search_simple_search_title" => "búsqueda sencilla",
     	"search_advanced_search_title" => "Búsqueda Avanzada",
     	"search_simple_search_intro" => "Haz una investigación dependiendo del apodo/alias/nombre:",
-    	"search_simple_search_ex" => 'Por ejemplo, si escribe "fou", obtendrá todos #5 #4 los resultados relacionados con Lefou o Fougasse.',
+        "search_simple_search_ex" => 'Por ejemplo, si escribe "fou", obtendrá todos los resultados relacionados con Lefou o Fougasse.',
         "search_advanced_search_intro" => "Agregue algunos criterios para realizar una búsqueda BDD. Es necesario <b>modificar al menos un criterio</b> para aplicar una investigación. No es necesario modificar todos los parámetros..",
         "search_min_max_fleet_pts_com" => "Puedes dejarlo vacío",
         "search_min_max_def_pts_com" => "Puedes dejarlo vacío",
